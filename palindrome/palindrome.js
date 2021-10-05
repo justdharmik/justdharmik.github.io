b91201e12971ca6af5cac3f77abce81c1bdb8b94
Dharmik Babariya <81213533+justdharmik@users.noreply.github.com>
2021-10-05 14:06:41 +0530

    Initial

---
+++ palindrome/palindrome.js
@@ -0,0 +1,28 @@
+
+const textip = document.querySelector("input")
+
+function check() {
+    if (textip.value == ''){
+        document.getElementById("print").innerHTML = `<br>Please check your input.`
+    }
+    else {
+        const text = textip.value.toLocaleLowerCase()
+        const nospace = text.replace(/ /g, "")
+        const tlist = text.split("")
+        const rev = tlist.reverse()
+        const revtext = rev.join("")
+        const extras = `<br>Words: ${text.split(" ").length} | Letters: ${nospace.length}`
+        const yes = `${text} = ${revtext}<br>It is a Palindrome! ${extras}`
+        const no = `${text} != ${revtext}<br>It is not a Palindrome! ${extras}`
+        if (text == revtext){
+            document.getElementById("print").innerHTML = yes
+            console.log("It is a palindrome")    
+            document.querySelector("input").value = ''    
+        }
+        else {
+            document.getElementById("print").innerHTML = no
+            console.log("It is not a palindrome")
+            document.querySelector("input").value = ''
+        }
+    }
+}
